62ea31028cfa9a7a400929fe2b56248010e5bdb4
epikapa <epikapa@live.com>
2015-12-30 17:17:36 -0500

    Added the templates and layout. Also init commit

---
+++ incident-db/incident-db.py
@@ -0,0 +1,20 @@
+from flask import *
+import os
+import sys
+import json
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('main.html')
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+
