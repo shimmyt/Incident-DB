6ba93b120d957b3862193db71ca738e515218348
Franklin Ji <Franklin.Ji@aexp.com>
2016-01-04 15:09:12 -0500

    added stuff

--- incident-db/incident-db.py
+++ incident-db/incident-db.py
@@ -3,6 +3,7 @@ import os
 import datetime
 import sys
 import json
+import requests
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
@@ -13,13 +14,16 @@ def main():
 
 @app.route("/test")
 def test():
-    return render_template("test.html")
+    try:
+        res = requests.get("http://localhost:9200/").status_code
+    except:
+        res = 404
+    return render_template("test.html", res=res)
 
 @app.route("/eventadd", methods=['GET', 'POST'])
 def add_event_page():
     if request.method == 'POST':
         es = Elasticsearch()
-        count = es.count(index='test', doc_type='event')['count']
         data = {}
         data['description'], data['date'], data['tags'] = request.form['description'], request.form['date'], request.form['tags']
         
@@ -27,15 +31,61 @@ def add_event_page():
         if not data['description']:
             data['description'] = "None"
         if not data['date']:
-            data['date'] = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            data['date'] = datetime.datetime.now()#.strftime("%m/%d/%Y %H:%M:%S")
+        else:
+            data['date'] = datetime.datetime.strptime(data['date'], "%m/%d/%Y %H:%M %p")
+            
         #TODO: add ability to upload json and parse 
         try:
-            print(es.index(index="test", doc_type='event', id=count, body=data))
+             results = add_to_event(es, data)
         except Exception as e:
             flash('indexing failed')
             return render_template("addevent.html")
-        flash('Data added successfully')
+        
+        flash('Data added successfully. (id=%s)' %(results['_id']))
     return render_template("addevent.html")
+
+@app.route("/json_upload", methods=['GET', 'POST'])
+def upload_json():
+    es = Elasticsearch()
+    ALLOWED_EXT = set(['json'])
+    if request.method == 'POST':
+        f = request.files['filez']
+        if f and allowed_file(f.filename, ALLOWED_EXT):
+            d = json.loads(f.read().encode("string-escape"))
+            try:
+                for data in d:
+                    add_to_event(es, data)
+            except:
+                flash("Indexing failed. Check upload file")
+                return render_template("json_upload.html")
+
+        flash("File successfully indexed")
+    return render_template("json_upload.html")
+
+@app.route("/json", methods=['GET', 'POST'])
+def to_json():
+    es = Elasticsearch()
+    d = []
+    results = es.search(index='test', body={"query": {"match_all": {}}})
+    for hit in results['hits']['hits']:
+        print d.append(hit['_source'])    
+    return json.dumps(d)
+    #return render_template("json.html", data=d)
+    
+'''FROM HERE UNDER SHOULD BE MOVED TO utils.py '''
+def allowed_file(filename, ALLOWED_EXT):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXT
+
+def add_to_event(es, data):
+    return es.index(index="test", doc_type='event', id=index_count(es)+1, body=data)
+    
+
+def index_count(es):
+     return es.count(index='test', doc_type='event')['count'] 
+        
+    
+    
 
 
 if __name__ == '__main__':
