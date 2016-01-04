a6a68e4e4c36089521142433f5f4002f1107be65
epikapa <epikapa@live.com>
2016-01-04 02:29:14 -0500

    added event adding function

--- incident-db/incident-db.py
+++ incident-db/incident-db.py
@@ -1,8 +1,9 @@
 from flask import *
 import os
+import datetime
 import sys
 import json
-import psycopg2
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 
@@ -14,10 +15,31 @@ def main():
 def test():
     return render_template("test.html")
 
-@app.route("/test2")
-def test2():
-    return render_template("test2.html")
+@app.route("/eventadd", methods=['GET', 'POST'])
+def add_event_page():
+    if request.method == 'POST':
+        es = Elasticsearch()
+        count = es.count(index='test', doc_type='event')['count']
+        data = {}
+        data['description'], data['date'], data['tags'] = request.form['description'], request.form['date'], request.form['tags']
+        
+        #if nothing was supplied, give defaults
+        if not data['description']:
+            data['description'] = "None"
+        if not data['date']:
+            data['date'] = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        #TODO: add ability to upload json and parse 
+        try:
+            print(es.index(index="test", doc_type='event', id=count, body=data))
+        except Exception as e:
+            flash('indexing failed')
+            return render_template("addevent.html")
+        flash('Data added successfully')
+    return render_template("addevent.html")
+
+
 if __name__ == '__main__':
+    app.secret_key = '01'
     app.run(host='0.0.0.0', debug=True)
 
 
